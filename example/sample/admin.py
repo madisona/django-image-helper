af84f5c4fac6a2dea56e46399e308c498978dfae
Aaron Madison <aaron.l.madison@gmail.com>
2012-03-17 07:46:09 -0500

    created admin image preview widget

--- example/sample/admin.py
+++ example/sample/admin.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 
+from image_helper.fields import SizedImageField
+from image_helper.widgets import AdminImagePreviewWidget
+
 from sample.models import TestModel
 
 class TestModelAdmin(admin.ModelAdmin):
@@ -9,5 +12,10 @@ class TestModelAdmin(admin.ModelAdmin):
     def preview_thumbnail(self, obj):
         return '<img src="{}" />'.format(obj.image.thumbnail.url())
     preview_thumbnail.allow_tags = True
+
+
+    formfield_overrides = {
+        SizedImageField: {'widget': AdminImagePreviewWidget},
+    }
 
 admin.site.register(TestModel, TestModelAdmin)
---
+++ image_helper/widgets.py
@@ -0,0 +1,25 @@
+
+from django.contrib.admin.widgets import AdminFileWidget
+from django.utils.safestring import mark_safe
+
+
+class AdminImagePreviewWidget(AdminFileWidget):
+    """
+    An admin widget that shows a preview of currently selected image.
+    """
+
+    def __init__(self, attrs=None, storage=None):
+        super(AdminImagePreviewWidget, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None):
+        content = super(AdminImagePreviewWidget, self).render(name, value, attrs)
+        return mark_safe(content + self._get_preview_tag(value))
+
+    def _get_preview_tag(self, value):
+        if value and hasattr(value, "url"):
+            return '<p class="file-upload current-file-preview">Current Preview:<br /><img src="{0}" style="max-width: 300px;" /></p>'.format(value.url)
+        return ''
+
+
+
+
