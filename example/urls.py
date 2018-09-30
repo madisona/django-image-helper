e7194e3e3b61c61ba2836114a1577b9d9869e731
Aaron Madison <aaron.l.madison@gmail.com>
2018-09-29 20:38:00 -0500

    Dependency: upgrading to django2 compatibility

--- example/urls.py
+++ example/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
 
 # serve media files for sample stuff to work.
